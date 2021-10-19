0c66bd4298a76c0a9a07414d2335df62f29a7219
ChrisAmelia <chris@objectdev.io>
2021-10-19 15:32:23 +0200

    fix(diagnostic): set new highlight groups
    LspDiagnosticsDefault* -> Diagnostic*
    LspDiagnosticsSign* -> DiagnosticSign

--- lua/telescope/make_entry.lua
+++ lua/telescope/make_entry.lua
@@ -931,6 +931,9 @@ function make_entry.gen_from_ctags(opts)
   end
 end
 
+local diagnostic_sign = (vim.fn.has('nvim-0.6') == 1) and "DiagnosticSign" or "LspDiagnosticsSign"
+local diagnostic_type = (vim.fn.has('nvim-0.6') == 1) and "Diagnostic" or "LspDiagnosticsDefault"
+
 function make_entry.gen_from_lsp_diagnostics(opts)
   opts = opts or {}
 
@@ -943,7 +946,7 @@ function make_entry.gen_from_lsp_diagnostics(opts)
       -- pcall to catch entirely unbound or cleared out sign hl group
       if type(severity) == "string" then
         local status, sign = pcall(function()
-          return vim.trim(vim.fn.sign_getdefined("LspDiagnosticsSign" .. severity)[1].text)
+             return vim.trim(vim.fn.sign_getdefined(diagnostic_sign.. severity)[1].text)
         end)
         if not status then
           sign = severity:sub(1, 1)
@@ -973,7 +976,7 @@ function make_entry.gen_from_lsp_diagnostics(opts)
     local pos = string.format("%4d:%2d", entry.lnum, entry.col)
     local line_info = {
       (signs and signs[entry.type] .. " " or "") .. pos,
-      "LspDiagnosticsDefault" .. entry.type,
+      diagnostic_type .. entry.type,
     }
 
     return displayer {
